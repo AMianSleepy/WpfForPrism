f608496e02c07819a0382b4eabdc4e307e2a8fcd
ShiYi <AMian_Sleepy@qq.com>
2025-10-16 11:50:07 +0800

    添加项目文件。

---
+++ MyToDo/App.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace MyToDo
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : PrismApplication
+    {
+        /// <summary>
+        /// 设置启动项
+        /// </summary>
+        /// <returns></returns>
+        /// <exception cref="NotImplementedException"></exception>
+        protected override Window CreateShell()
+        {
+            return Container.Resolve<Views.MainWindow>();
+        }
+
+        /// <summary>
+        /// 注入服务
+        /// </summary>
+        /// <param name="containerRegistry"></param>
+        protected override void RegisterTypes(IContainerRegistry containerRegistry)
+        {
+            
+        }
+    }
+
+}
---
+++ MyToDo/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,56 @@
+﻿using MyToDo.View;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace MyToDo.ViewModels
+{
+    internal class MainWindowViewModel : BindableBase
+    {
+        public DelegateCommand<string> ShowContentCmm { get; set; }
+
+        public MainWindowViewModel()
+        {
+            ShowContentCmm = new DelegateCommand<string>(ShowContentFunc);
+        }
+
+        /// <summary>
+        /// 改变显示用户控件
+        /// </summary>
+        /// <param name="viewName"></param>
+        private void ShowContentFunc(string viewName)
+        {
+            if (viewName == "UserControlA")
+            {
+                ShowContent = new UserControlA();
+            }
+            if (viewName == "UserControlB")
+            {
+                ShowContent = new UserControlB();
+            }
+            if (viewName == "UserControlC")
+            {
+                ShowContent = new UserControlC();
+            }
+        }
+
+        /// <summary>
+        /// 显示的内容
+        /// </summary>
+        private UserControl _ShowContent;
+
+        /// <summary>
+        /// 显示的内容
+        /// </summary>
+        public UserControl ShowContent
+        {
+            get { return _ShowContent; }
+            set { _ShowContent = value;
+                RaisePropertyChanged();
+            }
+        }
+    }
+}
