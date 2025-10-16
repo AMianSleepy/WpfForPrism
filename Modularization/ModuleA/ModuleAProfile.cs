a86e2f802c67e01389e0943d7eedeeaac19f4cd7
ShiYi <AMian_Sleepy@qq.com>
2025-10-16 19:50:19 +0800

    模拟导入他人撰写的模块，先创建了ModuleA项目和ModuleB项目模拟拿到了他人的模块
    （以下为Copilot生成）
    
    新增模块化支持，添加 ModuleA 和 ModuleB 项目
    
    新增了 Modularization 文件夹，并在解决方案中添加了 ModuleA 和 ModuleB 两个模块项目：
    - ModuleA 和 ModuleB 项目均基于 .NET 6.0，启用了 WPF 支持。
    - 两个模块分别添加了 Prism.DryIoc 包以支持依赖注入。
    - ModuleA 定义了 ViewA 用户控件及其导航注册。
    - ModuleB 定义了 ViewB 用户控件及其导航注册。

---
+++ Modularization/ModuleA/ModuleAProfile.cs
@@ -0,0 +1,28 @@
+﻿using ModuleA.Views;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ModuleA
+{
+    public class ModuleAProfile : IModule
+    {
+        public void OnInitialized(IContainerProvider containerProvider)
+        {
+            
+        }
+
+        /// <summary>
+        /// Registers application-specific types with the provided container registry.
+        /// </summary>
+        /// <remarks>Call this method during application startup to configure dependency injection. Types
+        /// registered here will be available for resolution throughout the application's lifetime.</remarks>
+        /// <param name="containerRegistry">The container registry used to register types for dependency injection. Cannot be null.</param>
+        public void RegisterTypes(IContainerRegistry containerRegistry)
+        {
+            containerRegistry.RegisterForNavigation<ViewA>();
+        }
+    }
+}
